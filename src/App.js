46505119b854b52c11f84349f4773df03ef3e111
minkisim <minkisim@naver.com>
2021-11-18 22:08:13 +0900

    first team project commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+/* 이 부분은 글자 혹은 이미지 깨지는 부분 없음 */
+
+import React,{useState} from 'react';
+import './App.css';
+import MainPage from './pages/MainPage';
+
+
+function App() {
+  return (
+    <div className="App">
+         <MainPage />  
+    </div>
+  )
+}
+export default App;
